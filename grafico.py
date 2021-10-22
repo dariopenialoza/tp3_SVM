b11eb0cd022b5cd785bde7fbe15e808a1e080487
Dario Alejandro Penialoza <dariopenialoza@gmail.com>
2021-10-22 20:31:15 -0300

    Pruebas finales

--- grafico.py
+++ grafico.py
@@ -5,7 +5,7 @@ from scipy import misc
 import matplotlib.pyplot as plt
 from PIL import Image
 
-from sklearn.svm import SVC
+from sklearn.svm import SVC, LinearSVC
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import accuracy_score
@@ -23,14 +23,20 @@ vacaA = np.array(vaca)
 vacaM = np.reshape(vacaA,(-1,3))
 
 cieloDF = pd.DataFrame(cieloM,columns=list('rgb'))
-cieloDF = cieloDF.assign(Tipo=-1)
+cieloDF = cieloDF.assign(Tipo=1)
 pastoDF = pd.DataFrame(pastoM,columns=list('rgb'))
-pastoDF = pastoDF.assign(Tipo=1)
+pastoDF = pastoDF.assign(Tipo=-1)
 vacaDF = pd.DataFrame(vacaM,columns=list('rgb'))
 vacaDF = vacaDF.assign(Tipo=-1)
 
 dataset = cieloDF.append(pastoDF,ignore_index=True)
 dataset = dataset.append(vacaDF,ignore_index=True)
+
+#carga de la imagen a clasificar
+cow = Image.open('ww.jpg')
+cowA = np.array(cow)
+cowM = np.reshape(cowA,(-1,3))
+cowDF = pd.DataFrame(cowM,columns=list('rgb'))
 
 # Preprocesado y modelado
 # ==============================================================================
@@ -46,46 +52,46 @@ color_train, color_test, tipo_train, tipo_test = train_test_split(
                                     )
 # Creación del modelo SVM lineal
 # ==============================================================================
-model = SVC(C = 10, kernel = 'linear' , random_state=123)
+model = LinearSVC(C = 10  ,  random_state=123)
 modelo = model.fit(color_train, tipo_train)
 
 # Predicción valores grid
-predicciones = modelo.predict(color_test)
+predicciones = modelo.predict(cowDF)
 
 # Creamos la figura
 fig2 = plt.figure()
 ax2 = fig2.add_subplot( 111, projection='3d')
 
-x = color_test.r
-y = color_test.g
-z = color_test.b
+x = cowDF.r
+y = cowDF.g
+z = cowDF.b
 ax2.scatter3D(x, y, z,
               c=predicciones, marker='o',
-              depthshade=True, s=2,
+              depthshade=False, s=2,
               cmap=plt.cm.tab20,
               edgecolors='gray',
               linewidth=0.1,)
 
-ax2.set_title("¿Es tipo pasto?, C = 10, kernel = 'Linear'")
+ax2.set_title("¿Es tipo cielo?, C = 10, kernel = 'Linear'")
 ax2.set_xlabel('R')
 ax2.set_ylabel('G')
 ax2.set_zlabel('B')
 ax2.legend(predicciones)
 
-# Vectores soporte
-ax2.scatter3D(
-    modelo.support_vectors_[:, 0],
-    modelo.support_vectors_[:, 1],
-    modelo.support_vectors_[:, 2],
-    s=10, linewidth=0.5,
-    facecolors='none', edgecolors='red'
-)
+#Vectores soporte
+# ax2.scatter3D(
+#     modelo.support_vectors_[:, 0],
+#     modelo.support_vectors_[:, 1],
+#     modelo.support_vectors_[:, 2],
+#     s=10, linewidth=0.5,
+#     facecolors='none', edgecolors='red'
+# )
 
 #Hiperplano
 z = lambda x,y: (-modelo.intercept_[0]-modelo.coef_[0][0]*x -modelo.coef_[0][1]*y) / modelo.coef_[0][2]
 tmp = np.linspace(0,255,25)
 x,y = np.meshgrid(tmp,tmp)
-ax2.plot_surface(x, y, z(x,y),alpha=0.5)
+ax2.plot_surface(x, y, z(x,y),alpha=0.9, cmap='Reds')
 
 plt.show()
 
